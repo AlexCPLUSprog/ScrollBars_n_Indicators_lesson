e4bdcc318bd87d2eda24dcaccb30cc68035ed3e8
Алекс <Alexandr_enid@mail.ru>
2023-07-05 20:59:19 +0500

    Добавьте файлы проекта.

---
+++ ScrollBars_n_Indicators/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace ScrollBars_n_Indicators
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progBarDemo = new System.Windows.Forms.ProgressBar();
+            this.btnStartPB = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.hScrollBar2 = new System.Windows.Forms.HScrollBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(9, 405);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(226, 36);
+            this.hScrollBar1.TabIndex = 0;
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.LargeChange = 20;
+            this.vScrollBar1.Location = new System.Drawing.Point(25, 201);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(29, 184);
+            this.vScrollBar1.TabIndex = 1;
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(243, 137);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 98);
+            this.textBox1.TabIndex = 2;
+            // 
+            // progBarDemo
+            // 
+            this.progBarDemo.Location = new System.Drawing.Point(38, 46);
+            this.progBarDemo.Name = "progBarDemo";
+            this.progBarDemo.Size = new System.Drawing.Size(259, 23);
+            this.progBarDemo.TabIndex = 3;
+            // 
+            // btnStartPB
+            // 
+            this.btnStartPB.Location = new System.Drawing.Point(332, 46);
+            this.btnStartPB.Name = "btnStartPB";
+            this.btnStartPB.Size = new System.Drawing.Size(75, 23);
+            this.btnStartPB.TabIndex = 4;
+            this.btnStartPB.Text = "Старт";
+            this.btnStartPB.UseVisualStyleBackColor = true;
+            this.btnStartPB.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnStartPB_MouseClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(445, 137);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(201, 98);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 2;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            65536});
+            this.numericUpDown1.Location = new System.Drawing.Point(314, 334);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            120,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(314, 378);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(259, 45);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Value = 5;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // hScrollBar2
+            // 
+            this.hScrollBar2.Location = new System.Drawing.Point(505, 270);
+            this.hScrollBar2.Name = "hScrollBar2";
+            this.hScrollBar2.Size = new System.Drawing.Size(278, 20);
+            this.hScrollBar2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 450);
+            this.Controls.Add(this.hScrollBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnStartPB);
+            this.Controls.Add(this.progBarDemo);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.vScrollBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDoubleClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progBarDemo;
+        private System.Windows.Forms.Button btnStartPB;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.HScrollBar hScrollBar2;
+    }
+}
+
---
+++ ScrollBars_n_Indicators/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace ScrollBars_n_Indicators
+{    
+    public partial class Form1 : Form
+    {
+        public Form Tools;
+        Form2 newForm = new Form2();
+        public Form1()
+        {
+            InitializeComponent();
+            ShowToolTip();
+            
+        }
+        public void ShowToolTip()
+        {
+            System.Windows.Forms.ToolTip myToolTip = new System.Windows.Forms.ToolTip();
+            myToolTip.AutoPopDelay = 5000; // Время отображения
+            myToolTip.InitialDelay = 1000; // Начальная задержка
+            myToolTip.ReshowDelay = 100; // Задержка при повторном наведении
+            
+            myToolTip.ShowAlways = /*false */true; // Показывать подсказки когда
+                                                   // родительское окно не активно
+            //myToolTip.ShowAlways = false /*true*/;
+
+            //myToolTip.IsBalloon = true;
+
+            myToolTip.SetToolTip(this.vScrollBar1, "Перемещение объекта TextBox по вертикали");
+            myToolTip.SetToolTip(this.hScrollBar1, "Нужно сделать TextBox перемещение объекта по горизонтали");
+            myToolTip.SetToolTip(pictureBox1, "Картинка");
+       }
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            
+            vScrollBar1.Minimum = 0;            
+            vScrollBar1.Maximum = 120;
+            textBox1.Location = new Point(textBox1.Location.X,
+                vScrollBar1.Value + 10);
+            
+            textBox1.Text = $"{textBox1.Location.X} : {textBox1.Location.Y}";
+        }
+
+
+        private void btnStartPB_MouseClick(object sender, MouseEventArgs e)
+        {
+            Timer timerPB = new Timer();
+            timerPB.Enabled = true;
+            timerPB.Interval = 1000;
+            timerPB.Tick += TimerPB_Tick;
+        }
+        
+        private void TimerPB_Tick(object sender, EventArgs e)
+        {
+            btnStartPB.Text = progBarDemo.Value.ToString();
+            progBarDemo.PerformStep();
+            //Text = ((Timer)sender).Interval.ToString();
+            
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            
+            //numericUpDown1.
+            var X = 445 + (int)numericUpDown1.Value;
+            pictureBox1.Location = new Point(X, pictureBox1.Location.Y);
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            int Y = 137 + trackBar1.Value * 10;
+            pictureBox1.Location = new Point(pictureBox1.Location.X, Y);
+        }
+
+        private void createElements(Form _form)
+        {
+            TextBox tbTools = new TextBox();
+            tbTools.Location = new Point(10,10);
+            tbTools.Size = new Size(50, 10);
+            _form.Controls.Add(tbTools);
+        }
+        private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            
+            if (Tools != null )
+            {
+                Tools.Dispose();
+                Tools.Close();
+                Tools = null;
+                newForm.Hide();
+            }
+            else
+            {
+                Tools = new Form();
+                createElements(Tools);
+                Tools.Text = "Настройки";
+                Tools.Show();
+                newForm.Show();
+            }
+        }
+
+        
+    }
+}
